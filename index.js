67520e1a706c2a522e0f73a3781a6a2877c519f2
Dario Barrionuevo <dariobafran@gmail.com>
2023-09-16 12:26:10 +0200

    BlogListBAckend step1

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+const MONGODB_URI = process.env.MONGODB_URI;
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
   likes: Number,
 });
-const mockBlogs = [
-  {
-    title: " Vamos que nos vamos",
-    author: "pepe",
-    url: "wwww.eeeee.com",
-    likes: 2,
-  },
-  {
-    title: "Una vieja y un viejo van",
-    author: "luis",
-    url: "wwww.aaaaa.es",
-    likes: 4,
-  },
-];
+
 const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = "mongodb://localhost/bloglist";
-mongoose.connect(mongoUrl);
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
 app.use(cors());
 app.use(express.json());
@@ -37,10 +37,9 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/blogs", (request, response) => {
-  //   Blog.find({}).then((blogs) => {
-  //     response.json(blogs);
-  //   });
-  response.json(mockBlogs);
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
 });
 
 app.post("/api/blogs", (request, response) => {
@@ -50,8 +49,9 @@ app.post("/api/blogs", (request, response) => {
     response.status(201).json(result);
   });
 });
+app.use(unknownEndpoint);
 
-const PORT = 3003;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
