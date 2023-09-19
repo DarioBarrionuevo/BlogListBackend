ae4042073798ff4bfc6483efbf9b6ffd1a2bea6b
Dario Barrionuevo <dariobafran@gmail.com>
2023-09-19 16:51:23 +0200

    REfactor with async await

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,29 +2,33 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
 // GET ALL
-blogsRouter.get("/", (request, response, next) => {
-  // console.log("eeeeeeeeeeee")
-  Blog.find({})
-    .then((blogs) => {
-      response.json(blogs);
-    })
-    .catch((error) => next(error));
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
+
+  // Blog.find({})
+  //   .then((blogs) => {
+  //     response.json(blogs);
+  //   })
+  // .catch((error) => next(error));
 });
 // ADD ONE
-blogsRouter.post("/", (request, response, next) => {
+blogsRouter.post("/", async (request, response) => {
   const blog = new Blog(request.body);
 
   if (!blog.title || !blog.author || !blog.url) {
     response.status(400).end();
   } else {
-    blog
-      .save()
-      .then((result) => {
-        response.status(201).json(result);
-      })
-      .catch((error) => {
-        next(error);
-      });
+    const result = await blog.save();
+    response.status(201).json(result);
+    // blog
+    //   .save()
+    //   .then((result) => {
+    //     response.status(201).json(result);
+    //   })
+    // .catch((error) => {
+    //   next(error);
+    // });
   }
 });
 
