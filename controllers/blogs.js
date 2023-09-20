10934ffceefb633bffd7175444ea49af528ae4d5
Dario Barrionuevo <dariobafran@gmail.com>
2023-09-20 17:51:56 +0200

    Blog list expansions step3 and step5

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,9 +1,14 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 // GET ALL
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", {
+    username: 1,
+    name: 1,
+    id: 1,
+  });
   response.json(blogs);
 });
 // GET ONE
@@ -17,14 +22,23 @@ blogsRouter.get("/:id", async (request, response) => {
 });
 // ADD ONE
 blogsRouter.post("/", async (request, response) => {
-  const blog = new Blog(request.body);
+  const body = request.body;
+  const users = await User.find({});
+  const user = users[0];
 
-  if (!blog.title || !blog.author || !blog.url) {
-    response.status(400).end();
-  } else {
-    const result = await blog.save();
-    response.status(201).json(result);
-  }
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id,
+  });
+
+  const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+
+  response.json(savedBlog);
 });
 // DELETE ONE
 blogsRouter.delete("/:id", async (request, response) => {
